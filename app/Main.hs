5c0bd8a5def80adf0038cf946e997599fcc87ef5
Michael Dippery <michael@monkey-robot.com>
2022-11-29 12:10:09 -0800

    Show result of single die roll when running `roll`
    This is just a stub for the eventual functionality of this program.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,16 @@
 module Main (main) where
 
+import System.Environment (getArgs)
+import HighRoller.Gaming
+
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = do
+  argv <- getArgs
+  case argv of
+    [desc] ->
+      case splitDice desc of
+        Just (1, d) -> do
+          n <- rollIO d
+          putStrLn $ show n
+        _           -> return ()
+    _      -> return ()
